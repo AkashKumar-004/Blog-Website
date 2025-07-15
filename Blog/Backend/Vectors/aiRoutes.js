1216eb5b927fed3a0ef4cf82415c1de25550bce8
Akashkumar S <s.akashkumar004@gmail.com>
2025-07-15 23:33:55 +0530

    Delete Blog directory

--- Blog/Backend/Vectors/aiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateSummary } = require('../Controller/aiController');
-
-router.post('/generate-summary', generateSummary);
-
-module.exports = router;
